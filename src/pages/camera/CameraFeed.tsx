14cbb62f7081ff789e02f6c75934adc30dd8a46e
Ofori01 <oforidarkwah7@gmail.com>
2026-02-13 11:40:12 +0000

    feat: added camera feed info modal

--- src/pages/camera/CameraFeed.tsx
+++ src/pages/camera/CameraFeed.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { cn } from "../../lib/utils";
-import { Info } from "lucide-react";
+import { CircleX, Info } from "lucide-react";
 
 import cameraImage from "../../assets/camera_feed.png";
+import { ToggleSwitch, ValueDisplay } from "../../components/ui";
 
 interface CameraFeedProps {
   className?: string;
@@ -14,6 +15,8 @@ const CameraFeed: React.FC<CameraFeedProps> = ({
   className,
   cameraLocation,
 }) => {
+  const [isInfoOpen, setInfoOpen] = useState(false);
+
   return (
     <div
       className={cn(
@@ -31,7 +34,37 @@ const CameraFeed: React.FC<CameraFeedProps> = ({
             <p className="tracking-wide ">Live Feed</p>
           </div>
         </div>
-        <Info className="size-5 text-primary" />
+        <div className="relative">
+          <Info
+            className="size-5 text-primary cursor-pointer"
+            onClick={() => setInfoOpen(!isInfoOpen)}
+          />
+          {/* Info modal */}
+          <div
+            className={`absolute  top-6 right-0 flex  flex-col gap-1 backdrop-blur-md bg-black/15 rounded-md w-72 transition-opacity transition-discrete ease-in-out duration-1000 ${isInfoOpen ? "flex" : "hidden"} `}
+          >
+            <div className="inline-flex justify-between items-center px-2 ">
+              <h3 className="tracking-wide text-[12px]">Info : </h3>
+              <CircleX
+                className="size-3.5 cursor-pointer"
+                onClick={() => setInfoOpen(!isInfoOpen)}
+              />
+            </div>
+
+            <hr className="bg-gray-400 h-0.5" />
+            <div className="inline-flex justify-between items-center px-2">
+              <p className="tracking-wide text-[12px] ">Camera Recording</p>
+              <ToggleSwitch checked onChange={() => {}} />
+            </div>
+
+            <div className="inline-flex justify-between items-center px-2">
+              <h3 className="tracking-wide text-[12px]">
+                FPS Camera Recording
+              </h3>
+              <ValueDisplay value="-100+" />
+            </div>
+          </div>
+        </div>
       </div>
       {/* Todo: will replace with video feed -> React player */}
       <div className="absolute inset-0 bg-amber-200 h-full w-full ">
