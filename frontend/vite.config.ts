b11f91b254f41363a6b9dc6f8965dc3779ab7fd0
Ofori01 <oforidarkwah7@gmail.com>
2026-03-03 11:40:45 +0000

    fix: removed video streaming from proxy. Causing slow streamings

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,8 +13,9 @@ export default defineConfig({
     },
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://localhost:8000",
         changeOrigin: true,
+        secure: false, // allow self-signed cert
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
